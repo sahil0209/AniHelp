c453d7b4341d2ad5f83fea719460bef0c6be9405
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2021-01-31 21:18:39 +0530

    final

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 
 const PORT = process.env.PORT === undefined ? 3000 : process.env.PORT;
 app.use(express.static("build"));
+app.get("*", (_, res) => {
+	res.send(express.static("build"));
+});
 app.listen(PORT, () => {
 	console.log(`Served on PORT = ${PORT}`);
 });
